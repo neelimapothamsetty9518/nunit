9ee69166d09c6bcdf601a7e53245fe294cb228a6
neelimapothamsetty9518 <neelimapothamsetty1@gmail.com>
2024-02-19 19:16:51 +0530

    initial commit

---
+++ Class.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assignmentfeb6mvc.Models
+{
+    public class Class
+    {
+        public int Cid { get; set; }
+        public string CName { get; set; }
+    }
+}
---
+++ Subject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assignmentfeb6mvc.Models
+{
+    public class Subject
+    {
+
+        public int Sid { get; set; }
+        public string SName { get; set; }
+        public string Marks { get; set; }
+
+    }
+}
